3b78cdc7d98b4cc0c9b4833f93da10e8cc058bc2
Disha Chouhan <dishachouhan255@gmail.com>
2023-10-24 17:31:04 +0530

    Angular UI FIRST COMMIT

---
+++ src/app/bankruptcy-prediction/bankruptcy-prediction.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bankruptcy-prediction',
+  templateUrl: './bankruptcy-prediction.component.html',
+  styleUrls: ['./bankruptcy-prediction.component.css']
+})
+export class BankruptcyPredictionComponent {
+
+}
---
+++ src/app/profit-prediiction/profit-prediiction.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profit-prediiction',
+  templateUrl: './profit-prediiction.component.html',
+  styleUrls: ['./profit-prediiction.component.css']
+})
+export class ProfitPrediictionComponent {
+
+}
